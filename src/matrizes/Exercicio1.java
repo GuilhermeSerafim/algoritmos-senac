fef6780bc15697bc5a0ddc45e5c216d4aa310fa6
GUILHERME DA SILVA SERAFIM <guilherme.sserafim3@senacsp.edu.br>
2025-02-27 08:47:04 -0300

    Deixando mesmo exercicio de gerar matriz com numeros pseudorandomicos dinaico

--- src/matrizes/Exercicio1.java
+++ src/matrizes/Exercicio1.java
@@ -1,10 +1,17 @@
 package matrizes;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Exercicio1 {
     public static void main(String[] args) {
-        int[][] matriz = gerarMatriz(3, 4);
+        Scanner in = new Scanner(System.in);
+        int linhas, colunas;
+        System.out.println("Digite a quantidade de linhas");
+        linhas = in.nextInt();
+        System.out.println("Digite a quantidade de colunas");
+        colunas = in.nextInt();
+        int[][] matriz = gerarMatriz(linhas, colunas);
         imprimeMatriz(matriz);
     }
 
