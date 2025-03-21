3c5404e82632d933712ff865b8c687613978e527
GuilhermeSerafim <guilerstudies@gmail.com>
2025-03-21 20:24:40 -0300

    Realizando busca binária e busca linear em uma vetor unidimensional

--- src/sistemaDeBuscas/buscaBinaria.java
+++ src/sistemaDeBuscas/buscaBinaria.java
@@ -6,21 +6,56 @@ import java.util.Scanner;
 public class buscaBinaria {
 
     public static void main(String[] args) {
-        Random random = new Random();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Digite um número a ser procurado no arr: ");
-        int num = in.nextInt();
-        int[] arr = new int[100];
-        int numero = buscaLinear(arr, num);
-        System.out.println(numero);
-    }
+		Scanner in = new Scanner(System.in);
+		System.out.print("Digite o tamanho do vetor: ");
+		int tamanhoVetor = in.nextInt();
+		System.out.print("Digite o até que número o vetor irá preencher aleatoriamente: ");
+		int limite = in.nextInt();
+		int[] vetor = new int[tamanhoVetor];
+		populaArrNrsRandons(vetor, limite);
+		System.out.println("Vetor gerado!");
+		System.out.print("Digite o número que deseja buscar: ");
+		int numeroASerProcurado = in.nextInt();
+		int nrBuscadoBinario = buscaBinaria(vetor, numeroASerProcurado);
+		int nrBuscadoLinear = buscaLinear(vetor, numeroASerProcurado);
+		// Se caso houver pouca variedade de resultados, quem irá encontrar o número mais rápido é a busca linear
+		System.out.println("Busca linear: Número encontrado na posição " + nrBuscadoLinear);
+		System.out.println("Busca binária: Número encontrado na posição " + nrBuscadoBinario);
+		
+		in.close();
+		
+	}
 
-    public static int buscaLinear(int vetor[], int numeroASerProcurado) {
-        for(int i = 0; i < vetor.length; i++)  {
-            if(vetor[i] == numeroASerProcurado) {
-                return i;
-            }
-        }
-        return -1;
-    }
+	public static void populaArrNrsRandons(int[] vetor, int limite) {
+		Random random = new Random();
+		for (int i = 0; i < vetor.length; i++) {
+			vetor[i] = random.nextInt(limite + 1);
+		}
+	}
+
+	public static int buscaLinear(int vetor[], int numeroASerProcurado) {
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] == numeroASerProcurado) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int buscaBinaria(int vetor[], int numeroASerProcurado) {
+		int inicio = 0;
+		int fim = vetor.length - 1;
+		int meio;
+		while (inicio <= fim) {
+			meio = (inicio + fim) / 2;
+			if (vetor[meio] == numeroASerProcurado) {
+				return meio;
+			} else if (vetor[meio] < numeroASerProcurado) {
+				inicio = meio + 1;
+			} else {
+				fim = meio - 1;
+			}
+		}
+		return -1;
+	}
 }
