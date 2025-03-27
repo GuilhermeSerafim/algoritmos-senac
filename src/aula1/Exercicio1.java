00e66cacb59e37f192e69f092b4435e9eb150843
GuilhermeSerafim <guilerstudies@gmail.com>
2025-03-27 11:22:32 -0300

    Ajustes

--- src/aula1/Exercicio1.java
+++ src/aula1/Exercicio1.java
@@ -1,5 +1,6 @@
 package aula1;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,19 +17,9 @@ public class Exercicio1 {
         populaArrNrsRandons(arr, 20);
 
         bubbleSortParesSuperiores(arr);
+        System.out.println("Números ordenados por impar < par");
+        System.out.println(Arrays.toString(arr));
 
-        // Solicita ao usuário um número para buscar no array
-        System.out.println("Digite um número a ser buscado: ");
-        int nrProcurado = in.nextInt();
-
-        // Realiza busca binária no array ordenado
-        int indiceBuscado = buscaBinaria(arr, nrProcurado);
-
-        // Exibe o resultado da busca
-        if (indiceBuscado == -1)
-            System.out.println("Número não encontrado");
-        else
-            System.out.println("Número encontrado no índice: " + indiceBuscado);
     }
 
     public static void populaArrNrsRandons(int[] vetor, int limite) {
@@ -38,11 +29,12 @@ public class Exercicio1 {
         }
     }
 
+
     public static void bubbleSortParesSuperiores(int[] vetor) {
         int aux;
         for (int i = 0; i < vetor.length - 1; i++) {
             for (int j = 0; j < vetor.length - i - 1; j++) {
-                if (!ehPar(vetor[j]) && ehPar(vetor[j+1])) {
+                if (ehPar(vetor[j]) && !ehPar(vetor[j+1])) {
                     aux = vetor[j];
                     vetor[j] = vetor[j + 1];
                     vetor[j + 1] = aux;
@@ -55,32 +47,5 @@ public class Exercicio1 {
         return n% 2 == 0;
     }
 
-    public static int buscaLinear(int vetor[], int numeroASerProcurado) {
-        for (int i = 0; i < vetor.length; i++) {
-            if (vetor[i] == numeroASerProcurado) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
-    public static int buscaBinaria(int vetor[], int numeroASerProcurado) {
-        int inicio = 0;
-        int fim = vetor.length - 1;
-        int meio;
-
-        while (inicio <= fim) {
-            meio = (inicio + fim) / 2;
-
-            if (vetor[meio] == numeroASerProcurado) {
-                return meio;
-            } else if (vetor[meio] < numeroASerProcurado) {
-                inicio = meio + 1;
-            } else {
-                fim = meio - 1;
-            }
-        }
-
-        return -1;
-    }
 }
---
+++ src/aula1/Exercicio2.java
@@ -0,0 +1,8 @@
+package aula1;
+// Separe os números pares e ímpares do conjunto.
+// Em seguida, ordene os números pares em ordem crescente.
+// Após isso, ordene os números ímpares também em ordem crescente.
+// Por fim, junte os dois grupos, mantendo essa ordem.
+
+public class Exercicio2 {
+}
