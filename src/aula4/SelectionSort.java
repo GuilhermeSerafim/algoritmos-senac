1685b7ff7520c1745dc89a0026c4dc194c9a01c1
GuilhermeSerafim <guilerstudies@gmail.com>
2025-03-28 13:19:05 -0300

    Base selection sort

---
+++ src/aula4/SelectionSort.java
@@ -0,0 +1,33 @@
+package aula4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100000);
+        }
+
+        System.out.println(Arrays.toString(numbers));
+        selectionSort(numbers);
+    }
+
+    private static void selectionSort(int[] numbers) {
+        int length = numbers.length;
+        for (int i = 0; i < length; i++) {
+            int min = numbers[i];
+            int indexOfMin = i;
+            for (int j = i + 1; j < length; j++) {
+                if(numbers[j] < min) {
+                    min = numbers[j];
+                    indexOfMin = j;
+                }
+            }
+        }
+    }
+
+
+}
